07ed1578e3fcc841119d8a02fcb56437181fa3e3
Lavish Jain <lavishjain36@gmail.com>
2021-03-12 12:09:08 +0530

    Added test that returns area of Rectangle

--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -1,2 +1,55 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RectangleTest {
+
+
+    @Test
+    public  void  testAreaForPositiveValuedSide(){
+        int length=2,breadth=3;
+        Rectangle obj=new Rectangle(length,breadth);
+        int expectedArea=6;
+        assertEquals(expectedArea,obj.areaofRectangle());
+    }
+
+    @Test
+    public  void  testAreaForLengthNegativeValuedSide(){
+        int length=-2,breadth=3;
+        Rectangle obj=new Rectangle(length,breadth);
+        int expectedArea=-6;
+        assertEquals(expectedArea,obj.areaofRectangle());
+    }
+
+    @Test
+    public  void  testAreaForBreadthNegativeValuedSide(){
+        int length=2,breadth=-3;
+        Rectangle obj=new Rectangle(length,breadth);
+        int expectedArea=-6;
+        assertEquals(expectedArea,obj.areaofRectangle());
+    }
+    @Test
+    public  void  testAreaForBothNegativeValuedSide(){
+        int length=-2,breadth=-3;
+        Rectangle obj=new Rectangle(length,breadth);
+        int expectedArea=6;
+        assertEquals(expectedArea,obj.areaofRectangle());
+    }
+
+    @Test
+    public  void  testAreaForBothSidesValuedZero(){
+        int length=0,breadth=0;
+        Rectangle obj=new Rectangle(length,breadth);
+        int expectedArea=0;
+        assertEquals(expectedArea,obj.areaofRectangle());
+    }
+    @Test
+    public  void  testAreaForLengthValuedZero(){
+        int length=0,breadth=3;
+        Rectangle obj=new Rectangle(length,breadth);
+        int expectedArea=0;
+        assertEquals(expectedArea,obj.areaofRectangle());
+    }
+
+
 }
