7059484b1feb97711ffb2838c52e6c8ccdaa3e1c
Lavish Jain <lavishjain36@gmail.com>
2021-03-12 12:17:42 +0530

    Added tests that returns area and perimeter of Rectangle

--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -51,5 +51,60 @@ public class RectangleTest {
         assertEquals(expectedArea,obj.areaofRectangle());
     }
 
+    @Test
+    public void testPerimeterForLengthAsValuedSides(){
+        int length = -2;
+        int breadth = 3;
+        Rectangle obj = new Rectangle(length,breadth);
+        int expectedPerimeter = 2;
 
+        assertEquals(expectedPerimeter,obj.perimeterofRectangle());
+    }
+
+    @Test
+    public void testPerimeterForBreadthAsValuedSides(){
+        int length = 2;
+        int breadth = -3;
+        Rectangle obj = new Rectangle(length,breadth);
+        int expectedPerimeter = -2;
+
+        assertEquals(expectedPerimeter,obj.perimeterofRectangle());
+    }
+    @Test
+    public void testPerimeterForBothSidesAsValuedSides(){
+        int length = -2;
+        int breadth = -3;
+        Rectangle obj = new Rectangle(length,breadth);
+        int expectedPerimeter = -10;
+
+        assertEquals(expectedPerimeter,obj.perimeterofRectangle());
+    }
+
+    @Test
+    public void testPerimeterForLengthValuedZero(){
+        int length = 0;
+        int breadth = 4;
+        Rectangle obj = new Rectangle(length,breadth);
+        int expectedPerimeter = 8;
+
+        assertEquals(expectedPerimeter,obj.perimeterofRectangle());
+    }
+    @Test
+    public void testPerimeterForBreadthValuedZero(){
+        int length = 4;
+        int breadth = 0;
+        Rectangle obj = new Rectangle(length,breadth);
+        int expectedPerimeter = 8;
+
+        assertEquals(expectedPerimeter,obj.perimeterofRectangle());
+    }
+    @Test
+    public void testPerimeterForBothSidesValuedZero(){
+        int length = 0;
+        int breadth = 0;
+        Rectangle obj = new Rectangle(length,breadth);
+        int expectedPerimeter = 0;
+
+        assertEquals(expectedPerimeter,obj.perimeterofRectangle());
+    }
 }
