5dc500fed3476e0701bdcaea006f1ef2d0205aa1
Lavish Jain <lavishjain36@gmail.com>
2021-03-12 11:54:51 +0530

    added functionality that return area and perimeter of rectangle

---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,17 @@
+public class Rectangle {
+    int length;
+    int breadth;
+
+    public Rectangle (int length,int breadth){
+        this.length=length;
+        this.breadth=breadth;
+
+    }
+
+    public  int area(){
+        return  length*breadth;
+    }
+    public  int perimeter(){
+        return  2*(length+breadth);
+    }
+}
